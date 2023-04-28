4444b779471e41cdff3eae5f37634e96abda27ad
alec strobel <astrobel96@googlemail.com>
2023-04-28 23:55:58 +0200

    Added responsiveness to all pages

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import NavController from "./nav_controller"
+application.register("nav", NavController)
+
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
 
