2eb585805f108a48c5bdf001a24c129868c107f6
alec strobel <astrobel96@googlemail.com>
2023-04-29 14:23:13 +0200

    Added force SSL to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # HTTPS
+  config.force_ssl = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
